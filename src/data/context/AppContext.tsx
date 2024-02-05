9ece7ff8b3078b23628a8d85a617bdb73666e05a
Raphael Braga <raffael.info@gmail.com>
2024-02-05 17:45:29 -0300

    :rocket: feat: local storage para salvar tema

--- src/data/context/AppContext.tsx
+++ src/data/context/AppContext.tsx
@@ -1,20 +1,27 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 type themeValue = 'dark' | '';
 
 interface AppContextProps {
-  theme?: themeValue;
+  theme?: string;
   alternarTema?: () => void;
 }
 
 const AppContext = createContext<AppContextProps>({});
 
 export function AppProvider(props) {
-  const [tema, setTema] = useState<themeValue>('dark');
+  const [tema, setTema] = useState('dark');
 
   function alternarTema() {
+    const novoTema = tema === '' ? 'dark' : '';
     setTema(tema === '' ? 'dark' : '');
+    localStorage.setItem('tema', novoTema);
   }
+
+  useEffect(() => {
+    const temaSalvo = localStorage.getItem('tema');
+    setTema(temaSalvo);
+  }, []);
 
   return (
     <AppContext.Provider
